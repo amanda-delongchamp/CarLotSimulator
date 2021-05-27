46fb27ca6f5e673e89456558ef3ec763c4bd5e40
amanda-delongchamp <amanda.delongchamp@gmail.com>
2021-05-26 17:02:03 -0700

    Exercise 2 Static

--- CarLotSimulatorApp/CarLotSimulator/CarLot.cs
+++ CarLotSimulatorApp/CarLotSimulator/CarLot.cs
@@ -6,10 +6,19 @@ namespace CarLotSimulator
 {
     public class CarLot
     {
+
+        public static int numberOfCars;
+        public List<Car> Cars = new List<Car>();
+
+        static CarLot()
+        {
+            
+        }
+        
         //Create a CarLot class
         //It should have at least one property: a List of cars
 
-        public List<Car> Cars = new List<Car>();
+        
 
         
         //Instanciate the a Carlot at the beginning of the program and as you create a car add the car to the list.
